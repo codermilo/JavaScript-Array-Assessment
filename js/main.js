ebfddf00c0aaef726f3616acfd1a1031619d9c59
Dan <milocodes.io@gmail.com>
2025-04-14 16:11:21 +0100

    imported Sass, started html layout and css styling. Noted JS outline

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+/*
+Structure
+- show image (https://picsum.photos/200/300)
+- save that image with id (https://picsum.photos/id/237/200/300)
+- (optional) have buttons for grayscale, blur and square images
+- input field for emails
+- dropdown select for emails (different from the input field?) (Save emails in local storage. Array of objects? )
+- on submission save image to email-linked array (in local-storage)
+- generate new image on new image button click
+- have array of generated images and track index number (Can traverse previously generated images with arrow buttons) (Limit 15)
+- render images saved to a user's array
+
+- conditionals to show account settings or main app
+- extra checks in submission to check if valid email
+
+********* use a randomly generated id number (max 1050) and then apply that to the url for the image and that way I can call the photo's info as well to store it... *********
+*/
+
+/* Functions to generate new image, to save the image in local array, save index, render images based on the index, save button states for blur and grayscale */
+
+/* Functions to grab array of users from local storage, to check if submitted email is there, to add that email to the array, to add any new images to that user's array (or delete) */
+
+/* Functions to show user's images if button selected on smaller screen sizes */
