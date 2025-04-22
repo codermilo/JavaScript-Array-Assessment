c28972417171de931f574bb07e9b2b43d9963d7e
Dan <milocodes.io@gmail.com>
2025-04-22 09:33:38 +0100

    Initial draft complete

--- js/main.js
+++ js/main.js
@@ -5,41 +5,26 @@ const $loginRoute = $('#login-route');
 const $registerBtn = $('form');
 const $emailInput = $('#email');
 $registerBtn.on('submit', (event) => {
-    console.log('register button clicked');
     event.preventDefault();
 
     emailSubmit($emailInput.val());
-    console.log(getUsers());
-    console.log('$emailInput.val()', $emailInput.val())
 })
 
 const $deleteBtn = $('#delete');
 $deleteBtn.click(() => {
-    console.log('delete button clicked');
     deleteUser($emailInput.val());
-})
-
-const $loginBtn = $('#login');
-$loginBtn.click(() => {
-    console.log('login button clicked');
-    logInUser('dan_friend@hotmail.co.uk');
-    console.log(getUsers());
 })
 
 const $logoutBtn = $('#logout');
 $logoutBtn.click(() => {
-    console.log('logout button clicked');
     logOutUsers();
-    console.log(getUsers());
 })
 
 // Function to save main-img to user's gallery
 const $saveBtn = $('#save');
 $saveBtn.click((id) => {
-    console.log('save button clicked');
     addImage(id);
 })
-
 
 // Button functionality to change main image
 $nextBtn = $('#next');
@@ -47,6 +32,18 @@ $previousBtn = $('#previous');
 
 // Loader component
 $loader = $('.loading-container');
+
+
+const $mainImg = $('#main-img');
+// Get size of parent container to generate image dimensions (Can use this for future function additions)
+const px = { h: 1920, w: 1080 };
+
+// Variable to store array of random generated images
+const imgArray = []
+let index = 0;
+
+
+//--------------- Auth ---------------//
 
 // Function to style page so auth route is shown
 function showAuthRoute() {
@@ -87,11 +84,9 @@ function setUsers(users) {
 function checkUserEmail(email) {
     // Retrieve users
     const usersArray = getUsers();
-    console.log('usersArray in checkUserEmail', usersArray);
 
     // Map and look for submitted email
     const user = usersArray.find((user) => user.email === email);
-    console.log('user in checkUserEmail', user);
 
     return user;
 }
@@ -106,8 +101,6 @@ function deleteUser(email) {
 
         // Return list of users that exclude the given user
         const filteredUsers = usersArray.filter((user) => user.email !== email);
-        console.log("email given in deleteUser param", email);
-        console.log("filteredEmails in deleteUsers", filteredUsers);
 
         // Set users to filteredUsers
         setUsers(filteredUsers);
@@ -181,7 +174,6 @@ function emailSubmit(email) {
             console.log('User already exists:', doesUserExist);
             return new Error('User with this email already exists');
         } else {
-            console.log('doesUserExist', doesUserExist);
             // Log out users so we can log in new user
             logOutUsers();
 
@@ -198,37 +190,6 @@ function emailSubmit(email) {
     }
 
 
-}
-
-// Function to add image to logged in user's imageArray
-function addImage() {
-    // Get the url currently used as background for main-img. Should be same as imgArray[index]
-    const url = imgArray[index];
-
-    // Get users so I can filter and modify list
-    const users = getUsers();
-    const changedUsers = users.map((userObj) => {
-        if (userObj.isLoggedIn === true) {
-            // Check if user already has image url already saved
-            const hasImage = userObj.savedImages.find((img) => img === url);
-            console.log('hasImage: ', hasImage);
-            console.log('url in addImage: ', url)
-            if (!hasImage) {
-                return {
-                    ...userObj,
-                    savedImages: [url, ...userObj.savedImages]
-                };
-            } else {
-                console.log('User has already saved that image!');
-                return userObj;
-            }
-        } else {
-            return userObj;
-        }
-    });
-    setUsers(changedUsers);
-    // Refresh user's img gallery 
-    getUserGallery();
 }
 
 // Function to append login accounts to sign in section
@@ -250,16 +211,13 @@ $userList.on("click", 'li', function () {
 // Populate existing users 
 function populateEmailList() {
     const users = getUsers();
-    console.log('users in populateEmailList', users);
     if (users && users.length > 0) {
-        console.log('populating user list');
         let userListEl = $('<ul></ul>');
         users.forEach((user, i) => {
             userListEl.append(`<li id="user-${i}"><span>${user.email}</span><button class="delete-btn">Delete</button></li>`);
         })
         $userList.html(userListEl);
     } else {
-        console.log('returning empty user list');
         $userList.html(`<p id='empty-list'>There are no email collections currently stored.. Please create a new user!</p>`);
     }
 }
@@ -274,7 +232,6 @@ function loginCheck() {
         showAuthRoute();
     } else
         if (loggedInUsers.length === 0) {
-            console.log('No logged in user');
             hideAuthRoute();
         } else {
             console.log('Error: there are multiple logged in users, logging off');
@@ -283,9 +240,137 @@ function loginCheck() {
         }
 }
 
+
 // Call on page load
 populateEmailList();
 loginCheck();
+setMainImage();
+
+//--------------- Main Image ---------------//
+
+// Function to add image to logged in user's imageArray
+function addImage() {
+    // Get the url currently used as background for main-img. Should be same as imgArray[index]
+    const url = imgArray[index];
+
+    // Get users so I can filter and modify list
+    const users = getUsers();
+    const changedUsers = users.map((userObj) => {
+        if (userObj.isLoggedIn === true) {
+            // Check if user already has image url already saved
+            const hasImage = userObj.savedImages.find((img) => img === url);
+            if (!hasImage) {
+                return {
+                    ...userObj,
+                    savedImages: [url, ...userObj.savedImages]
+                };
+            } else {
+                console.log('User has already saved that image!');
+                return userObj;
+            }
+        } else {
+            return userObj;
+        }
+    });
+    setUsers(changedUsers);
+    // Refresh user's img gallery 
+    getUserGallery();
+}
+
+// Function can be used to generate random image ids or retrieve specific ones from array
+async function generateUrl(index) {
+    if (index !== undefined) {
+        const url = imgArray[index];
+        return url;
+    } else {
+        // Generate random url from picsum
+        let response = await fetch(`https://picsum.photos/${px.h}/${px.w}.webp`);
+        if (!response.ok) {
+            throw new Error(`Error fetching new image: ${response.status}`);
+        }
+        return response.url;
+    }
+}
+
+// Function to populate or change main image. Pass param to use image from array
+async function setMainImage(change) {
+
+    // Disable buttons immediately while image changes and show loader
+    setButtonsEnabled(false);
+    $loader.addClass('visible');
+
+    let url;
+    // If you pass a param (i.e. to view a previously generated image)
+    if (change) {
+        // If param is forward then generate a new image and attach it to array as well
+        if (change == "next") {
+            index++;
+            // If index is longer than imgArray then generate a new image
+            if (index >= imgArray.length) {
+                url = await generateUrl();
+                imgArray.push(url);
+            } else {
+                // Get the URL of imgArray[index]
+                url = await generateUrl(index);
+            }
+
+        } else if (change == "previous") {
+            // If index is lower than 0 then generate new id and unshift it onto imgArray
+            if (index <= 0) {
+                // Don't change index if it's at 0
+                url = await generateUrl();
+                imgArray.unshift(url);
+            } else {
+                index--;
+                url = await generateUrl(index);
+            }
+        }
+    } else {
+        // Generate random image when no param passed (Usually on window load)
+        if (index !== 0) {
+            index++;
+        }
+        url = await generateUrl();
+        imgArray.push(url);
+    }
+    // Preload via Image() and only swap & re‑enable on load
+    // Create new image element but don't attach it to the dom
+    const img = new Image();
+    // On img load set the mainImg background to the new URL
+    img.onload = () => {
+        $mainImg.css('background-image', `url(${url})`);
+        setButtonsEnabled(true);
+        $loader.removeClass('visible');
+    };
+    img.onerror = () => {
+        // Error catch
+        console.error('Failed to load', url);
+        setButtonsEnabled(true);
+        $loader.removeClass('visible');
+    };
+    // Async fetch URL and when download completes call onload
+    img.src = url;
+}
+
+
+// Generate new image and send it to image array
+$nextBtn.click(() => {
+    setMainImage('next');
+})
+$previousBtn.click(() => {
+    setMainImage('previous');
+})
+
+// Function to enable/disable main image buttons
+function setButtonsEnabled(isEnabled) {
+    $nextBtn.prop('disabled', !isEnabled);
+    $previousBtn.prop('disabled', !isEnabled);
+}
+
+
+
+
+//--------------- User gallery ---------------//
 
 // Function get user's profile and render their images
 function getUserGallery() {
@@ -304,7 +389,7 @@ function getUserGallery() {
 
             // Create saved-img-inner. Set the background to img
             const inner = `<a href="${img}"><div class="saved-img-inner" style="background-image: url(${img})"></div></a>`;
-            const deleteBtn = '<button class="delete-btn">X</button>';
+            const deleteBtn = '<button class="delete-btn">&times;</button>';
 
             // Push the element to html
             const imageEl = '<div class="saved-img-container">' + inner + deleteBtn + '</div>';
@@ -361,112 +446,5 @@ $imageGallery.on("click", '.delete-btn', function (event) {
     // Finally call getUserGallery to update user's gallery
     getUserGallery();
 })
-
-const $mainImg = $('#main-img');
-// Get size of parent container to generate image dimensions (Can use this for future function additions)
-const px = { h: 1920, w: 1080 };
-
-// Function can be used to generate random image ids or retrieve specific ones from array
-async function generateUrl(index) {
-    if (index !== undefined) {
-        const url = imgArray[index];
-        return url;
-    } else {
-        // Generate random url from picsum
-        let response = await fetch(`https://picsum.photos/${px.h}/${px.w}.webp`);
-        if (!response.ok) {
-            throw new Error(`Error fetching new image: ${response.status}`);
-        }
-        console.log('url', response.url);
-        return response.url;
-    }
-}
-
-// Variable to store array of random generated images
-const imgArray = []
-let index = 0;
-
-// Function to populate or change main image. Pass param to use image from array
-async function setMainImage(change) {
-
-    // Disable buttons immediately while image changes and show loader
-    setButtonsEnabled(false);
-    $loader.addClass('visible');
-
-    let url;
-    // If you pass a param (i.e. to view a previously generated image)
-    if (change) {
-        // If param is forward then generate a new image and attach it to array as well
-        if (change == "next") {
-            index++;
-            // If index is longer than imgArray then generate a new image
-            if (index >= imgArray.length) {
-                url = await generateUrl();
-                imgArray.push(url);
-            } else {
-                // Get the URL of imgArray[index]
-                url = await generateUrl(index);
-            }
-
-        } else if (change == "previous") {
-            // If index is lower than 0 then generate new id and unshift it onto imgArray
-            if (index <= 0) {
-                // Don't change index if it's at 0
-                url = await generateUrl();
-                imgArray.unshift(url);
-            } else {
-                index--;
-                url = await generateUrl(index);
-            }
-        }
-    } else {
-        // Generate random image when no param passed (Usually on window load)
-        if (index !== 0) {
-            index++;
-        }
-        url = await generateUrl();
-        imgArray.push(url);
-    }
-    // Preload via Image() and only swap & re‑enable on load
-    // Create new image element but don't attach it to the dom
-    const img = new Image();
-    // On img load set the mainImg background to the new URL
-    img.onload = () => {
-        $mainImg.css('background-image', `url(${url})`);
-        setButtonsEnabled(true);
-        $loader.removeClass('visible');
-    };
-    img.onerror = () => {
-        console.error('Failed to load', url);
-        // you might want to re‑enable or show an error state
-        setButtonsEnabled(true);
-        $loader.removeClass('visible');
-    };
-    // Async fetch URL and when download completes call onload
-    img.src = url;
-}
-
-
-// Set main image on load
-setMainImage();
-
-// Generate new image and send it to image array
-$nextBtn.click(() => {
-    console.log('next button clicked');
-    setMainImage('next');
-})
-$previousBtn.click(() => {
-    console.log('previous button clicked');
-    setMainImage('previous');
-})
-
-// Function to enable/disable main image buttons
-function setButtonsEnabled(isEnabled) {
-    $nextBtn.prop('disabled', !isEnabled);
-    $previousBtn.prop('disabled', !isEnabled);
-}
-
-
-
 
 
